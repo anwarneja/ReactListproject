3ccb2f3d9dd4cf227157c64bbfe6dba322e0023d
anwarneja <anwarneja55@gmail.com>
2025-05-05 21:36:07 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>My List</h1>
+        <ul>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
